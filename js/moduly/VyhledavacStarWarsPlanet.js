22aa9ec3a778f5435e8d957294604119b926dfe8
JackJayJames <ciklops@seznam.cz>
2022-12-14 10:50:13 +0100

    selectory elementu

--- js/moduly/VyhledavacStarWarsPlanet.js
+++ js/moduly/VyhledavacStarWarsPlanet.js
@@ -2,5 +2,17 @@
 
 
 export class VyhledavacStarWarsPlanet{
-    
+    constructor(){
+
+        this.inputElement = document.querySelector("#input");
+        this.buttonElement = document.querySelector("#vyhledat-tlacitko");
+        this.vypis = document.querySelector("#vypis");
+
+        this.kontrola();
+    }
+    kontrola(){
+        console.log(this.inputElement);
+        console.log(this.buttonElement);
+        console.log(this.vypis);
+    }
 }
